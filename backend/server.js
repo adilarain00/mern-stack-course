b348798c70a288f0dcb40895ee0e65cd4a1e7dde
adilarain00 <addilarain00@gmail.com>
2025-02-11 16:22:19 +0500

    create a middleware for handeling errors

--- backend/server.js
+++ backend/server.js
@@ -15,3 +15,13 @@ app.use("/api/auth", authRouter)
 mongoose.connect(process.env.MONGO).then(() => console.log("Connected to MongoDB")).catch((err) => console.log(err));
 
 app.listen(5000, () => console.log("Server started on port 5000"));
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({ 
+        success: false,
+        statusCode, 
+        message
+     });
+});
